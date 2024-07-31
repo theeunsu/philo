1cd30b585b3acbb26f1311125f0feacfd856f1cd
Eunsu Ahn <theeunsu@gmail.com>
2024-07-31 17:41:51 +0200

    final

---
+++ init.c
@@ -0,0 +1,89 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   init.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/25 16:08:35 by eahn              #+#    #+#             */
+/*   Updated: 2024/07/30 12:28:07 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+// EVEN ODD fork assignment to avoid deadlock
+// philo_position: philo id - 1
+static void	init_fork(t_philo *philo, t_fork *forks, int philo_position)
+{
+	int	num_philos;
+
+	num_philos = philo->info->num_philos;
+	philo->first_fork = &forks[(philo_position + 1) % num_philos];
+	philo->second_fork = &forks[philo_position];
+	if (philo->id % 2 == 0)
+	{
+		philo->first_fork = &forks[philo_position];
+		philo->second_fork = &forks[(philo_position + 1) % num_philos];
+	}
+}
+
+// Initialize the information of the philos
+static int	init_philo(t_info *info)
+{
+	int		i;
+	t_philo	*philo;
+
+	i = -1;
+	info->philos = malloc(sizeof(t_philo) * info->num_philos);
+	info->forks = malloc(sizeof(t_fork) * info->num_philos);
+	if (!info->philos || !info->forks)
+		return (print_error("Malloc failed."));
+	safe_mutex_operation(&(info->info_mutex), INIT);
+	safe_mutex_operation(&(info->print_mutex), INIT);
+	while (++i < info->num_philos)
+	{
+		safe_mutex_operation(&(info->forks[i].fork_mutex), INIT);
+		philo = info->philos + i;
+		safe_mutex_operation(&(philo->philo_mutex), INIT);
+		info->forks[i].fork_id = i;
+		philo->id = i + 1;
+		philo->full_flag = false;
+		philo->meal_count = 0;
+		philo->info = info;
+		init_fork(philo, info->forks, i);
+	}
+	return (0);
+}
+
+// Initialize common information for philos
+// fail: -1 / success: 0
+// ms = 1/1000s |  µs = 1/1,000,000s | ns = 1/1,000,000,000s
+//                ms      ms      ms      ms
+// ./philo 5     800     200     200     200     [5]
+int	init_info(t_info *info, int ac, char **av)
+{
+	info->num_philos = ft_atoi(av[1]);
+	if (info->num_philos <= 0)
+		return (print_error("Number of philosophers must be greater than 0."));
+	info->time_to_die = ft_atoi(av[2]) * 1000;
+	info->time_to_eat = ft_atoi(av[3]) * 1000;
+	info->time_to_sleep = ft_atoi(av[4]) * 1000;
+	if (info->num_philos < 1 || info->time_to_die < 1 || info->time_to_eat < 1
+		|| info->time_to_sleep < 1)
+		return (print_error("Invalid argument."));
+	info->threads_counter = 0;
+	info->finish_flag = false;
+	info->all_ready_flag = false;
+	if (ac == 6)
+	{
+		info->must_eat_count = ft_atoi(av[5]);
+		if (info->must_eat_count < 1)
+			return (print_error("Invalid number of meals."));
+	}
+	else
+		info->must_eat_count = -1;
+	if (init_philo(info) == -1)
+		return (-1);
+	return (0);
+}
---
+++ safe_thread.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   safe_thread.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eahn <eahn@student.42.fr>                  +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/06/28 15:02:09 by eahn              #+#    #+#             */
+/*   Updated: 2024/07/28 23:04:38 by eahn             ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../inc/philo.h"
+
+// Wrapper for thread operations
+
+static void	thread_error(int status, t_opcode opcode)
+{
+	if (status == 0)
+		return ;
+	if (status == EAGAIN)
+		print_error("No resources to create another thread");
+	else if (status == EPERM)
+		print_error("The caller does not have appropriate permission .");
+	else if (status == EINVAL && opcode == CREATE)
+		print_error("The value specified by attr is invalid.");
+	else if (status == EINVAL && (opcode == JOIN || opcode == DETACH))
+		print_error("The thread is not a joinable thread.");
+	else if (status == ESRCH)
+		print_error("No thread could be found.");
+	else if (status == EDEADLK)
+		print_error("A deadlock was detected.");
+}
+
+// One function to handle all thread errors
+// create join detach
+void	safe_thread_operation(pthread_t *thread, void *(*foo)(void *),
+		void *data, t_opcode opcode)
+{
+	int	status;
+
+	status = 0;
+	if (CREATE == opcode)
+		thread_error(pthread_create(thread, NULL, foo, data), opcode);
+	else if (JOIN == opcode)
+		thread_error(pthread_join(*thread, NULL), opcode);
+	else if (DETACH == opcode)
+		thread_error(pthread_detach(*thread), opcode);
+	else
+	{
+		print_error("Use (CREATE | JOIN | DETACH)");
+		return ;
+	}
+	if (status != 0)
+	{
+		thread_error(status, opcode);
+		return ;
+	}
+}
